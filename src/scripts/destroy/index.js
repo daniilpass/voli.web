00882e416ff85be8bc02bd793fe63436b7c9ec32
Daniil <anil1641@mail.ru>
2022-10-17 20:41:09 +0300

    Destroy text

---
+++ src/scripts/destroy/index.js
@@ -0,0 +1,13 @@
+window.destroyTarget = function(element) {
+    
+    const rec = element.getBoundingClientRect();
+    element.style.transition = 'bottom 2s, transform 2s';
+    element.style.position = 'fixed';
+    element.style.bottom = `${window.innerHeight - rec.bottom}px`;
+
+    window.requestAnimationFrame(() => {
+        element.style.position = 'fixed';
+        element.style.bottom = `-${rec.height+rec.width}px`;  
+        element.style.transform = 'rotate(360deg)';
+    })    
+}
